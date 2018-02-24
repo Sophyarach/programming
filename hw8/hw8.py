cb78b88945ff60f1f806ec0f91ac8ff339a1751e
Sophyarach <31651154+Sophyarach@users.noreply.github.com>
2018-02-24 17:19:59 +0300

    Update hw8.py

--- hw8/hw8.py
+++ hw8/hw8.py
@@ -1,39 +1,35 @@
 import random
 
+def create_dict(file_name):
+    questions={}
+    with open(file_name,encoding="utf-8") as file:
+        for line in file:
+            word,hint=str(line.strip()).split(",")
+            questions.setdefault(hint,word)
+    return(questions)
 
-def dict_from_file(filename):
-    words = {}
-    with open(filename, encoding='utf-8') as f:
-        for line in f:
-            if line == 'Word,Hint\n':
-                continue
-            line = str(line.strip())
-            word, hint = line.split(',')
-            words.setdefault(word, hint)
-    return words
+def random_line(file_name):
+    with open(file_name,encoding="utf-8") as file:
+        lines=file.readlines()
+        randline=lines[random.randint(0, len(words)-1)]
+    return randline
 
+def question(questions):
+    word=random.choice(list(questions.keys()))
+    l=len(word)
+    dothint=""
+    for i in range (l):
+        dothint+="."
+    print(questions[word],dothint)
+    print ("Ваш ответ:")
+    answer=str(input())
+    if answer!=word:
+        return "Не угадали"
+    else:
+        return "Угадали!"
 
-def dots(word):
-    dots = ''
-    for i in range(len(word)):
-        dots += '.'
-    return dots
-
-
-def guessing_game(words):
-    word = random.choice(list(words.keys()))
-    print(words[word], dots(word))
-    print('Input your guess: ')
-    guess = str(input())
-    if guess != word:
-        return 'YOU LOST ;('
-    elif guess == word:
-        return'YOU WON ^-^'
-
-
-print('Input filename:')
-filename = input()
-print(guessing_game(dict_from_file(filename)))
-
-
+print("Введите имя файла:")
+file_name=input()
+d=create_dict(file_name)
+print(question(d))
 
